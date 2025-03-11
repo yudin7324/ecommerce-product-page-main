92d02fde99f31c20ffb85f9de5b974e041e7ad47
Ilya Yudin <yudin2402@gmail.com>
2025-03-11 20:14:45 +0100

    readme

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",   // Например, темно-синий
+        secondary: "#EAB308", // Желтый
+        accent: "#F43F5E",    // Красный
+      },
+    },
+  },
+  plugins: [],
+};
