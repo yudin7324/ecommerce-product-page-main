1ef6775664ff4eb5e4871b794520316685bcce37
Ilya Yudin <yudin2402@gmail.com>
2025-03-16 12:33:39 +0100

    header template implementerd

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,39 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import './header.scss'
 import logo from '@/assets/logo.svg'
 import avatar from '@/assets/avatar.png'
 import Navigation from '@/components/Navigation/Navigation'
 import IconCart from '@/components/icons/IconCart'
+import IconMenu from '@/components/icons/IconMenu'
 
 const Header:FC = () => {
+
+  const [isShowMenu, setIsShowMenu] = useState(false);
+
+  function toggleMenu() {
+    setIsShowMenu((prev) => !prev)
+  }
+
   return (
     <header className='header'>
       <div className='container'>
         <div className='header__wrap'>
 
+          <button 
+            className='header__menu' 
+            type='button' 
+            aria-label='menu button'
+            onClick={toggleMenu}
+            
+          >
+            <IconMenu />
+          </button>
+
           <div className='header__logo'>
             <img src={logo} alt="logo" />
           </div>
           
-          <Navigation />
+          <Navigation activeMenu={isShowMenu} handleToggleMenu={toggleMenu}/>
 
           <div className='header__cart'>
             <button 
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,27 +1,53 @@
 import { FC } from 'react'
 import './navigation.scss'
+import IconClose from '@/components/icons/IconClose'
 
-const Navigation: FC = () => {
+interface NavigationProps {
+  activeMenu: boolean;
+  handleToggleMenu: () => void;
+}
+
+const Navigation:FC<NavigationProps> = ({ activeMenu, handleToggleMenu }) => {
+
   return (
-    <nav className='navigation'>
-      <ul className='navigation__list'>
-        <li className='navigation__item'>
-          <a href="#" className='navigation__link'>Collections</a>
-        </li>
-        <li className='navigation__item'>
-          <a href="#" className='navigation__link'>Men</a>
-        </li>
-        <li className='navigation__item'>
-          <a href="#" className='navigation__link'>Women</a>
-        </li>
-        <li className='navigation__item'>
-          <a href="#" className='navigation__link'>About</a>
-        </li>
-        <li className='navigation__item'>
-          <a href="#" className='navigation__link'>Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className={`navigation ${activeMenu && 'active'}`}>
+        <div className='container'>
+     
+          <div className='navigation__list'>
+            <button 
+              className='navigation__close' 
+              type='button' 
+              aria-label='close menu button'
+              onClick={handleToggleMenu}
+            >
+              <IconClose />
+            </button>
+            <ul className='navigation__list-wrap'>
+              <li className='navigation__item'>
+                <a href="#" className='navigation__link'>Collections</a>
+              </li>
+              <li className='navigation__item'>
+                <a href="#" className='navigation__link'>Men</a>
+              </li>
+              <li className='navigation__item'>
+                <a href="#" className='navigation__link'>Women</a>
+              </li>
+              <li className='navigation__item'>
+                <a href="#" className='navigation__link'>About</a>
+              </li>
+              <li className='navigation__item'>
+                <a href="#" className='navigation__link'>Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div 
+          className={`navigation__backdrop ${activeMenu && 'active'}`} 
+          onClick={handleToggleMenu}
+        ></div>
+      </nav>
+    </>
   )
 }
 
